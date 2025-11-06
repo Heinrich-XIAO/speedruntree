ef018788288aa0d29454e29b82eb1a38a28b3249
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2025-11-05 20:00:33 -0600

    remove useless comments

--- convex/users.ts
+++ convex/users.ts
@@ -8,11 +8,6 @@ export const store = mutation({
       throw new Error("Called storeUser without authentication present");
     }
 
-    // Check if we've already stored this identity before.
-    // Note: If you don't want to define an index right away, you can use
-    // ctx.db.query("users")
-    //  .filter(q => q.eq(q.field("tokenIdentifier"), identity.tokenIdentifier))
-    //  .unique();
     const user = await ctx.db
       .query("users")
       .withIndex("by_token", (q) =>
@@ -20,13 +15,11 @@ export const store = mutation({
       )
       .unique();
     if (user !== null) {
-      // If we've seen this identity before but the name has changed, patch the value.
       if (user.name !== identity.name) {
         await ctx.db.patch(user._id, { name: identity.name });
       }
       return user._id;
     }
-    // If it's a new identity, create a new `User`.
     return await ctx.db.insert("users", {
       name: identity.name ?? "Anonymous",
       tokenIdentifier: identity.tokenIdentifier,
